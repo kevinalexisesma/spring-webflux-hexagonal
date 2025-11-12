1e2cb612c6c46db3809a60ba8182ad0bbf458c0f
Kevin Eslava <kevinalexisesma@ufps.edu.co>
2025-11-11 19:53:41 -0500

    Feat: Uploading project

---
+++ src/main/java/com/reactivo/onclass/app/on_class_reactivo/OnClassReactivoApplication.java
@@ -0,0 +1,13 @@
+package com.reactivo.onclass.app.on_class_reactivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnClassReactivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnClassReactivoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reactivo/onclass/app/on_class_reactivo/OnClassReactivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactivo.onclass.app.on_class_reactivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnClassReactivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
